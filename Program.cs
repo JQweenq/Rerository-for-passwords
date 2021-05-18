04e0e354750d372702da411cdb01782e65ca2f3c
Josty-Qweenq <josty.qweenq@gmail.com>
2021-05-18 21:40:26 +0700

    18.05.2021

--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using Base;
+
 
 namespace PG
 {
     class Program
     {
+        private static DataBase Base = new DataBase();
         static void Main(string[] args)
         {
+            Base.SignIn();
             Console.WriteLine("Hello World!");
         }
     }
---
+++ Scripts/Base.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Data.SQLite;
+using System.Data;
+
+namespace Base
+{
+    class DataBase
+    {
+        public void SignIn()
+        {
+            Console.WriteLine("SignIn");
+            
+            if (!File.Exists("Passwords.sqlite"))
+                SQLiteConnection.CreateFile("Passwords.sqlite");
+
+            try
+            {
+                SQLiteConnection MYCONN = new SQLiteConnection(@"Data Source=.\Passwords.sqlite;Version=3;");
+                MYCONN.Open();
+
+                SQLiteCommand MYCMD = new SQLiteCommand();
+                MYCMD.Connection = MYCONN;
+                MYCMD.CommandText = "CREATE TABLE IF NOT EXISTS Catalog (id INTEGER PRIMARY KEY AUTOINCREMENT, author TEXT, book TEXT)";                
+                MYCMD.ExecuteNonQuery();
+
+            }
+            catch (SQLiteException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }          
+        }
+    }
+}
