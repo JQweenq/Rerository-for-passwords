2ac58bf248056dff586ff581ade3496c21df8f17
Josty <josty.qweenq@gmail.com>
2021-05-23 21:35:30 +0300

    24.05.2021

--- Scripts/Base.cs
+++ Scripts/Base.cs
@@ -7,14 +7,17 @@ namespace Base
 {
     class DataBase
     {
-        private SQLiteConnection OpenBD()
+        // private string TABLE = ;
+        private SQLiteCommand MYCMD = new SQLiteCommand();
+        private SQLiteConnection MYCONN;
+        private string TABLE;
+        private void OpenBD()
         {
             if (!File.Exists("Passwords.sqlite"))
                 SQLiteConnection.CreateFile("Passwords.sqlite");
 
             try
             {
-                SQLiteConnection MYCONN;
                 MYCONN = new SQLiteConnection(@"Data Source=.\Passwords.sqlite;Version=3;");
                 MYCONN.Open();
 
@@ -22,42 +25,59 @@ namespace Base
                 SQLiteCommand MYCMD = new SQLiteCommand();
                 MYCMD.Connection = MYCONN;
                 */
-
-                return MYCONN;
+                
+                MYCMD.Connection = MYCONN;
             }
             catch (SQLiteException ex)
             {
                 Console.WriteLine("Error: " + ex.Message);
             }
 
-            return null;
         }
 
-        public void SignIn() // тестовая функция
+        public void SignIn(string LOGIN, string PASSWORD) // тестовая функция
         {
-            
+            OpenBD();
+            TABLE = $"{LOGIN}{PASSWORD}".ToLower();
         }
-
-        
         
         public void SingUp(string LOGIN, string PASSWORD) // добавление нового пользователя
         {
-            Console.WriteLine("SignUp");
-            SQLiteCommand MYCMD = new SQLiteCommand();
-            MYCMD.Connection = OpenBD();
-
-            MYCMD.CommandText = $"CREATE TABLE {LOGIN}{PASSWORD} (number, url, login, password, description)";
+            MYCMD.CommandText = $"CREATE TABLE {LOGIN}{PASSWORD} (url, login, password, description)";
             MYCMD.CommandType = CommandType.Text;
             MYCMD.ExecuteNonQuery();
         }
 
         public void AddPassword(string URL, string LOGIN, string PASSWORD, string DESCRIPTION = null) // добавление пароля для пользователя
         {
-            Console.WriteLine("SignIn");
-            
-            SQLiteCommand MYCMD = new SQLiteCommand();
-            MYCMD.Connection = OpenBD();
-            MYCMD.CommandText = $"INSERT INTO {LOGIN}{PASSWORD} (url, login, password, description) VALUES ('{URL}', '{LOGIN}', '{PASSWORD}', '{DESCRIPTION}')";
+            MYCMD.CommandText = $"INSERT INTO {TABLE} (url, login, password, description) VALUES ('{URL}', '{LOGIN}', '{PASSWORD}', '{DESCRIPTION}')";
+            MYCMD.CommandType = CommandType.Text;
+            MYCMD.ExecuteNonQuery();
+        }
+
+        public void RemoveAccount()
+        {
+            try
+            {
+                MYCMD.CommandText = $"DROP TABLE {TABLE}";
+                MYCMD.CommandType = CommandType.Text;
+                MYCMD.ExecuteNonQuery();
+            }
+            catch (SQLiteException)
+            {
+                Console.WriteLine("Error: {\n\tТакого аккаунта не существует\n}");
+            }
+        }
+        public void RemoveRow(int ROW = 1) // удаление строки
+        {
+            MYCMD.CommandText = $"DELETE FROM {TABLE} WHERE {ROW}";
+            MYCMD.CommandType = CommandType.Text;
+            MYCMD.ExecuteNonQuery();
+        }
+
+        public void RemoveAllRow()  // удаление всех строк
+        {
+            MYCMD.CommandText = $"DELETE FROM {TABLE}";
             MYCMD.CommandType = CommandType.Text;
             MYCMD.ExecuteNonQuery();
         }
